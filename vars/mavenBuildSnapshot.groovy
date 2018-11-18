36b494f668630f54b261f5b2732226902ab5d362
Yannick Menager <ymenager@kloudtek.com>
2018-11-18 09:12:16 -0800

    In progress

--- vars/mavenBuildSnapshot.groovy
+++ vars/mavenBuildSnapshot.groovy
@@ -1,12 +1,7 @@
-def call(args) {
-    echo "foo"
+def call(params) {
+    mvnCmd = params['mvnCmd']
+    if( mvnCmd == null ) {
+        mvnCmd = "-Dmaven.test.failure.ignore clean package"
+    }
+    echo "Building maven snapshot"
 }
-
-//def call(Map<String,String> params = [:]) {
-//    echo "XX"
-//    mvnCmd = params['mvnCmd']
-//    if( mvnCmd == null ) {
-//        mvnCmd = "-Dmaven.test.failure.ignore clean package"
-//    }
-//    echo "Building maven snapshot"
-//}
